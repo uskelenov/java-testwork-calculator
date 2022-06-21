bdcf9ee4f2061d60cdc5b0522c321822b83bfad9
uskelenov <uskelenovdeal@gmail.com>
2022-06-21 03:48:31 +0300

    test
    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,14 +19,14 @@ public class Main {
 
         // empty check
         if ( input.isEmpty() ) {
-            throw new InputException("throws Exception");
+            throw new InputException("throws Exception //т.к. пустая строка");
         }
 
-        // work with enter the string
+        // work with enter the String
         String mathExample = new String(input);
 
         if ( mathExample.length() < 3 ) {
-            throw new InputException("throws Exception");
+            throw new InputException("throws Exception //т.к. строка не является математической операцией");
         }
 
         // operator search
@@ -39,12 +39,13 @@ public class Main {
                     count++;
                 }
             }
-            if ( count == 1) {
+            if ( count == 1 ) {
                 searchOperator = operators.ADD.getSymbol();
+            } else if ( count > 1 ) {
+                throw new InputException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             } else {
-                throw new InputException("throws Exception");
+                throw new InputException("throws Exception //т.к. строка не является математической операцией");
             }
-
         } else if ( mathExample.contains( operators.SUBTRACT.getSymbol() ) ) {
             for ( char element : mathExample.toCharArray() ) {
                 if ( element == operators.SUBTRACT.getSymbol().charAt(0) ) {
@@ -53,8 +54,10 @@ public class Main {
             }
             if ( count == 1) {
                 searchOperator = operators.SUBTRACT.getSymbol();
+            } else if ( count > 1 ) {
+                throw new InputException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             } else {
-                throw new InputException("throws Exception");
+                throw new InputException("throws Exception //т.к. строка не является математической операцией");
             }
         } else if ( mathExample.contains( operators.MULTIPLY.getSymbol() ) ) {
             for ( char element : mathExample.toCharArray() ) {
@@ -64,8 +67,10 @@ public class Main {
             }
             if ( count == 1) {
                 searchOperator = operators.MULTIPLY.getSymbol();
+            } else if ( count > 1 ) {
+                throw new InputException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             } else {
-                throw new InputException("throws Exception");
+                throw new InputException("throws Exception //т.к. строка не является математической операцией");
             }
         } else if ( mathExample.contains( operators.DIVIDE.getSymbol() ) ) {
             for ( char element : mathExample.toCharArray() ) {
@@ -75,55 +80,205 @@ public class Main {
             }
             if ( count == 1) {
                 searchOperator = operators.DIVIDE.getSymbol();
+            } else if ( count > 1 ) {
+                throw new InputException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
             } else {
-                throw new InputException("throws Exception");
+                throw new InputException("throws Exception //т.к. строка не является математической операцией");
             }
         } else {
-            throw new InputException("throws Exception");
+            throw new InputException("throws Exception //т.к. строка не является математической операцией");
         }
 
         // operands search
         String firstOperand = mathExample.substring(0, mathExample.indexOf(searchOperator) );
         String secondOperand = mathExample.substring( mathExample.indexOf(searchOperator) + 1);
 
-        if ( Integer.parseInt(firstOperand) > 10 || Integer.parseInt(firstOperand) <= 0
-        || Integer.parseInt(secondOperand) > 10 || Integer.parseInt(secondOperand) <= 0 ) {
-            throw new InputException("throws Exception");
+        for ( roman symbolRoman : roman.values() )  {
+            if ( firstOperand.equals(symbolRoman.toString()) ) {
+                firstOperand = symbolRoman.name();
+            }
+
+            if ( secondOperand.equals(symbolRoman.toString()) ) {
+                secondOperand = symbolRoman.name();
+            }
         }
 
+        // number system check
+        Checks Check = new Checks();
+        if ( Check.isNumeric(firstOperand) != Check.isNumeric(secondOperand)
+        || Check.isNumeric(firstOperand) != Check.isNumeric(secondOperand) ) {
+            throw new InputException("throws Exception //т.к. используются одновременно разные системы счисления");
+        }
+        
         // getting a result
         String strResult;
         int result;
+        int fistRomanOperator = 0;
+        int secondRomanOperator = 0;
 
         switch ( searchOperator ) {
             case "+":
-                result = Integer.parseInt(firstOperand) + Integer.parseInt(secondOperand);
-                strResult = String.valueOf(result);
+
+                // roman result of ADD
+                if ( Check.isNumeric(firstOperand) == false) {
+                    for ( roman symbolRoman : roman.values()) {
+                        if ( firstOperand == symbolRoman.name() ) {
+                            fistRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+
+                        if ( secondOperand == symbolRoman.name() ) {
+                            secondRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+                    }
+
+                    if (fistRomanOperator == 0 || secondRomanOperator == 0) {
+                        throw new InputException("throws Exception //т.к. используются одновременно разные системы счисления");
+                    }
+
+                    result = fistRomanOperator + secondRomanOperator;
+                    strResult = String.valueOf(result);
+
+                    switchRoman switchRoman = new switchRoman();
+                    strResult = switchRoman.numRoman(strResult);
+                }
+
+                // arabic result of ADD
+                else {
+                    if ( Integer.parseInt(firstOperand) > 10 || Integer.parseInt(firstOperand) <= 0
+                    || Integer.parseInt(secondOperand) > 10 || Integer.parseInt(secondOperand) <= 0 ) {
+                        throw new InputException("throws Exception //т.к. на вход принимаются только числа от 1 до 10 и от I до X включительно");
+                    }
+
+                    result = Integer.parseInt(firstOperand) + Integer.parseInt(secondOperand);
+                    strResult = String.valueOf(result);
+                }
                 break;
 
             case "-":
-                result = Integer.parseInt(firstOperand) - Integer.parseInt(secondOperand);
-                strResult = String.valueOf(result);
+
+                // roman SUBTRACT result
+                if ( Check.isNumeric(firstOperand) == false) {
+                    for ( roman symbolRoman : roman.values()) {
+                        if ( firstOperand == symbolRoman.name() ) {
+                            fistRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+
+                        if ( secondOperand == symbolRoman.name() ) {
+                            secondRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+                    }
+
+                    if (fistRomanOperator == 0 || secondRomanOperator == 0) {
+                        throw new InputException("throws Exception //т.к. используются одновременно разные системы счисления");
+                    }
+
+                    result = fistRomanOperator - secondRomanOperator;
+
+                    if ( result < 0 ) {
+                        throw new InputException("throws Exception //т.к. в римской системе нет отрицательных чисел");
+                    }
+
+                    strResult = String.valueOf(result);
+
+                    switchRoman switchRoman = new switchRoman();
+                    strResult = switchRoman.numRoman(strResult);
+                }
+
+                // arabic SUBTRACT result
+                else {
+                    if ( Integer.parseInt(firstOperand) > 10 || Integer.parseInt(firstOperand) <= 0
+                            || Integer.parseInt(secondOperand) > 10 || Integer.parseInt(secondOperand) <= 0 ) {
+                        throw new InputException("throws Exception //т.к. на вход принимаются только числа от 1 до 10 и от I до X включительно");
+                    }
+
+                    result = Integer.parseInt(firstOperand) - Integer.parseInt(secondOperand);
+                    strResult = String.valueOf(result);
+                }
                 break;
 
             case "*":
-                result = Integer.parseInt(firstOperand) * Integer.parseInt(secondOperand);
-                strResult = String.valueOf(result);
+
+                // roman result MULTIPLY
+                if ( Check.isNumeric(firstOperand) == false) {
+                    for ( roman symbolRoman : roman.values() ) {
+                        if ( firstOperand == symbolRoman.name() ) {
+                            fistRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+
+                        if ( secondOperand == symbolRoman.name() ) {
+                            secondRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+                    }
+
+                    if (fistRomanOperator == 0 || secondRomanOperator == 0) {
+                        throw new InputException("throws Exception //т.к. используются одновременно разные системы счисления");
+                    }
+
+                    result = fistRomanOperator * secondRomanOperator;
+                    strResult = String.valueOf(result);
+
+                    switchRoman switchRoman = new switchRoman();
+                    strResult = switchRoman.numRoman(strResult);
+                }
+
+                // arabic result MULTIPLY
+                else {
+                    if ( Integer.parseInt(firstOperand) > 10 || Integer.parseInt(firstOperand) <= 0
+                            || Integer.parseInt(secondOperand) > 10 || Integer.parseInt(secondOperand) <= 0 ) {
+                        throw new InputException("throws Exception //т.к. на вход принимаются только числа от 1 до 10 и от I до X включительно");
+                    }
+
+                    result = Integer.parseInt(firstOperand) * Integer.parseInt(secondOperand);
+                    strResult = String.valueOf(result);
+                }
                 break;
 
             case "/":
-                if ( Integer.parseInt(secondOperand) == 0 ) {
-                    throw new InputException("throws Exception");
+
+                // roman DIVIDE result
+                if ( Check.isNumeric(firstOperand) == false) {
+                    for ( roman symbolRoman : roman.values()) {
+                        if ( firstOperand == symbolRoman.name() ) {
+                            fistRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+
+                        if ( secondOperand == symbolRoman.name() ) {
+                            secondRomanOperator = Integer.parseInt(symbolRoman.getSymbol());
+                        }
+                    }
+
+                    if (fistRomanOperator == 0 || secondRomanOperator == 0) {
+                        throw new InputException("throws Exception //т.к. используются одновременно разные системы счисления");
+                    }
+
+                    result = fistRomanOperator / secondRomanOperator;
+                    strResult = String.valueOf(result);
+
+                    switchRoman switchRoman = new switchRoman();
+                    strResult = switchRoman.numRoman(strResult);
                 }
 
-                result = Integer.parseInt(firstOperand) / Integer.parseInt(secondOperand);
-                strResult = String.valueOf(result);
+                // arabic DIVIDE result
+                else {
+                    if (Integer.parseInt(secondOperand) == 0) {
+                        throw new InputException("throws Exception //т.к. делить на 0 нельзя");
+                    }
+
+                    if ( Integer.parseInt(firstOperand) > 10 || Integer.parseInt(firstOperand) <= 0
+                            || Integer.parseInt(secondOperand) > 10 || Integer.parseInt(secondOperand) <= 0 ) {
+                        throw new InputException("throws Exception //т.к. на вход принимаются только числа от 1 до 10 и от I до X включительно");
+                    }
+
+                    result = Integer.parseInt(firstOperand) / Integer.parseInt(secondOperand);
+                    strResult = String.valueOf(result);
+                }
                 break;
             default:
-                throw new InputException("throws Exception");
+                throw new InputException("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
 
         return "The result of your math example: " + strResult;
+
     }
 
 }
@@ -131,7 +286,34 @@ public class Main {
 class InputException extends Exception {
 
     public InputException( String message ) {
+
         super( message );
+    }
+
+}
+
+class Checks {
+
+    boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
+
+class switchRoman {
+
+    String numRoman (String str) {
+        for ( romanResult symbolRoman : romanResult.values() ) {
+            if ( str.equals(symbolRoman.getSymbol()) ) {
+                str = symbolRoman.name();
+            }
+        }
+       return str;
     }
 
 }
--- src/com/company/roman.java
+++ src/com/company/roman.java
@@ -1,31 +1,15 @@
 package com.company;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+public enum roman {
+    I("1"), II("2"), III("3"), IV("4"), V("5"), VI("6"), VII("7"), VIII("8"), IX("9"), X("10");
 
-public class roman {
-    public static void main(String[] args) throws IOException {
-        try {
-            URL url = new URL("https://planetcalc.ru/378/?roman=&dec=2023");
+    private final String number;
 
-            try {
-                LineNumberReader reader = new LineNumberReader(new InputStreamReader(url.openStream()));
-                String string = reader.readLine();
-                while (string != null) {
-                    System.out.println(string);
-                    string = reader.readLine();
-                }
-                reader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    roman (String number) {
+        this.number = number;
+    }
 
-        } catch (MalformedURLException ex) {
-            ex.printStackTrace();
-        }
-
+    String getSymbol() {
+        return number;
     }
 }
